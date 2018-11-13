8eedbe05e8a2088d74a96677308b75b96a56455e
MonsterTechStudio <martinvegance@yahoo.com>
2018-11-13 14:56:04 +0800

    Initial Commit

---
+++ MTSNetworkingKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MTSNetworkingKit
+//
+//  Created by xiongyongjie on 2018/11/13.
+//  Copyright © 2018 MonsterTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
